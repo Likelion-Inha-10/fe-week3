e431955ce370e7bbd36b3d719ed2d8ab18acb8eb
Jina Park <wlsdk9803@naver.com>
2022-06-19 23:32:39 +0900

    주석 삭제

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// 해당 위치에 필요한 컴포넌트를 바탕으로 직접 Bootstrap Album 페이지를 개발하시면 됩니다!
 import Top from "./component/Top";
 import GlobalStyle from "./component/GlobalStyle";
 import PageExplain from "./component/PageExplain";
